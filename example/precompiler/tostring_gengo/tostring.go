8f07e908a808e9e930feedfb882d240a5280fda9
stevenzack <stevenzack@qq.com>
2019-04-15 16:44:11 +0800

    Just a Backup

--- example/precompiler/tostring_gengo/tostring.go
+++ example/precompiler/tostring_gengo/tostring.go
@@ -10,21 +10,21 @@ import (
 func Gen(g *gen.FileGenerator, gengoTag string, t reflect.Type) string {
 
 	str := `func (s *` + t.Name() + `) ToString () string {
-	return `
+	return "{"+`
 
 	for index := 0; index < t.NumField(); index++ {
 		field := t.Field(index)
 		kind := field.Type.Kind().String()
 		switch kind {
 		case "string":
-			str += "s." + field.Name + "+"
+			str += "s." + field.Name + `+","+`
 		default:
-			str += "fmt.Sprint(s." + field.Name + ")+"
+			str += "fmt.Sprint(s." + field.Name + `)+","+`
 			g.AddImport("fmt")
 		}
 	}
-
-	str = strings.TrimSuffix(str, "+")
+	str = strings.TrimSuffix(str, `","+`)
+	str += `"}"`
 
 	str += "\n}"
 
---
+++ genexecutor/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+
+	"github.com/StevenZack/tools/ioToolkit"
+
+	"github.com/StevenZack/gengo/gen"
+
+	"me/data"
+	"github.com/StevenZack/gengo/example/precompiler/tostring_gengo"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arg")
+		return
+	}
+
+	s := data.User{}
+	t := reflect.TypeOf(s)
+	g := &gen.FileGenerator{}
+	packageName := "data"
+
+	fo, e := os.OpenFile(os.Args[1], os.O_TRUNC|os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if e != nil {
+		fmt.Println(`create file error :`, e)
+		return
+	}
+
+	g.Writer = fo
+	fo.WriteString("package " + packageName + "\n")
+
+	genGoTag := ""
+	if len(os.Args) > 2 {
+		genGoTag = os.Args[2]
+	}
+	str := tostring_gengo.Gen(g, genGoTag, t)
+	g.WriteAllImports()
+	fo.WriteString(str)
+
+	fo.Close()
+	ioToolkit.RunAttachedCmd("gofmt", "-w", os.Args[1])
+}
